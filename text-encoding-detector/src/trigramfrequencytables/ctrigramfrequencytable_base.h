a2c958a0dfed535060e487a1da4bf56a2c9a7d13
VioletGiraffe <violet.giraffe@noreply.github.com>
2018-09-13 15:56:50 +0300

    Crash on macOs fixed

--- text-encoding-detector/src/trigramfrequencytables/ctrigramfrequencytable_base.h
+++ text-encoding-detector/src/trigramfrequencytables/ctrigramfrequencytable_base.h
@@ -5,6 +5,8 @@
 class CTrigramFrequencyTable_Base
 {
 public:
+	virtual ~CTrigramFrequencyTable_Base() = default;
+
 	inline const CTextParser::OccurrenceTable& trigramOccurrenceTable() const {return _table;}
 
 	virtual QString language() const = 0;
